87dc4b61e1539fddb2d8c895780175865e6e1d83
sinaenjuni <y2k4388@gmail.com>
2021-07-05 20:49:39 +0900

    define model and training process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from torch.nn import Module, Sequential
 from torch.utils.data import DataLoader
 
@@ -12,17 +13,33 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 print(torch.cuda.is_available())
+isUse_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if isUse_cuda else 'cpu')
 
 
-batch_size = 16
+###
+# Hyper-parameters
+###
+image_size = 28*28
+hidden_size = 256
+batch_size = 64
+latent_size = 64
+start_iters = 0
+max_iters = 100000
+sample_dir = 'samples'
+
+DATAPATH = Path.home() / 'dataset'
+SAVEPATH = Path('sample_dir')
+
+if not SAVEPATH.exists():
+    SAVEPATH.mkdir(exist_ok=True, parents=True)
+
 
 ###
 # Prepare MNIST dataset
 ###
 
 transform = Compose([ToTensor(), Normalize([.5], [.5])])
-
-DATAPATH = Path.home() / 'dataset'
 dataset = MNIST(root=DATAPATH, train=True, download=True, transform=transform)
 print(dataset[0][0].min(), dataset[0][0].max())
 
@@ -31,13 +48,109 @@ loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 iters = iter(loader)
 img, label = iters.next()
 
-# class Generator(Module):
-#     def __init__(self):
-#         self.main = Sequential([])
-#
-#     def forward(self, x):
+###
+# Define Models
+###
+
+# Discriminator
+D = nn.Sequential(
+    nn.Linear(image_size, hidden_size),
+    nn.LeakyReLU(0.2),
+    nn.Dropout(0.5),
+    nn.Linear(hidden_size, hidden_size),
+    nn.LeakyReLU(0.1),
+    nn.Dropout(0.5),
+    nn.Linear(hidden_size, 1),
+    nn.Sigmoid())
+
+# Generator
+G = nn.Sequential(
+    nn.Linear(latent_size, hidden_size),
+    nn.ReLU(),
+    nn.Dropout(0.1),
+    nn.Linear(hidden_size, hidden_size),
+    nn.ReLU(),
+    nn.Dropout(0.1),
+    nn.Linear(hidden_size, image_size),
+    nn.Tanh())
+
+# Device setting
+D = D.to(device)
+G = G.to(device)
 
 
+# Binary cross entropy loss and optimizer
+criterion = nn.BCELoss()
+d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0002)
+g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0002)
+
+
+def reset_grad():
+    d_optimizer.zero_grad()
+    g_optimizer.zero_grad()
+
+
+def denorm(x):
+    out = (x + 1) / 2
+    return out.clamp(0, 1)
+
+
+def getLatentVector(batch_size):
+    return torch.randn(batch_size, latent_size)
+
+
+for i in range(start_iters, max_iters):
+    iter_loader = iter(loader)
+    real_img, _ = iter_loader.next()
+    real_img = real_img.reshape(-1, image_size).to(device)
+
+    latent_input = getLatentVector(batch_size).to(device)
+
+    real_labels = torch.ones(batch_size, 1).to(device)
+    fake_labels = torch.zeros(batch_size, 1).to(device)
+
+    ###
+    # Train Discriminator (Optimizer Discriminator)
+    ###
+    real_outputs = D(real_img)
+    d_loss_real = criterion(real_outputs, real_labels)
+    real_score = real_outputs
+
+    # print(real_outputs)
+
+    fake_img = G(latent_input)
+    fake_outputs = D(fake_img)
+    d_loss_fake = criterion(fake_outputs, fake_labels)
+    fake_score = fake_outputs
+
+    d_loss = d_loss_real + d_loss_fake
+    reset_grad()
+    d_loss.backward()
+    d_optimizer.step()
+
+
+    ###
+    # Train Generator (Optimizer Generator)
+    ###
+
+    fake_img = G(latent_input)
+    fake_outputs = D(fake_img)
+    g_loss = criterion(fake_outputs, real_labels)
+
+    reset_grad()
+    g_loss.backward()
+    g_optimizer.step()
+
+    if (i+1) % 1000 == 0:
+        print('iters [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f}'
+              .format(i+1, max_iters,
+                      d_loss.item(), g_loss.item(),
+                      real_score.mean().item(), fake_score.mean().item()))
+
+        generated_img = fake_img.reshape(batch_size, 1, 28, 28).cpu()
+        grid = make_grid(denorm(generated_img))
+        plt.imshow(grid.permute(1,2,0))
+        plt.show()
 
 
 
