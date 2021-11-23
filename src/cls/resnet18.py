9d8903793a77e353fca6d76b2845c49197ba5ca2
sinaenjuni <y2k4388@gmail.com>
2021-11-23 10:46:55 +0900

    11.23.1

--- src/cls/resnet18.py
+++ src/cls/resnet18.py
@@ -1,4 +1,6 @@
 import os
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
@@ -24,8 +26,8 @@ tb = getTensorboard(tensorboard_path)
 
 # Hyper-parameters configuration
 num_epochs = 200
-batch_size = 32
-learning_rate = 0.0002
+batch_size = 64
+learning_rate = 0.002
 
 
 # Transformation define
@@ -81,7 +83,7 @@ tb.add_figure(tag='transformed_data_dist', figure=fig)
 
 
 # Data loader
-train_data_loader = torch.utils.data.DataLoader(dataset=transformed_dataset,
+train_data_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                           batch_size=batch_size,
                                           shuffle=True)
 
@@ -96,16 +98,26 @@ print(model)
 
 
 criterion = torch.nn.CrossEntropyLoss().to(device)  # 비용 함수에 소프트맥스 함수 포함되어져 있음.
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,
+                      momentum=0.9, weight_decay=5e-4)
 
 
 # Start training
-total_step = len(train_data_loader)
+num_train_step = len(train_data_loader)
+num_train = len(train_data_loader.dataset)
+
+num_test_step = len(test_data_loader)
+num_test = len(test_data_loader.dataset)
+best_loss = float("inf")
+
 for epoch in range(num_epochs):
     loss_train = 0
     acc_train = 0
+    total_train = 0
 
     for i, (images, labels) in enumerate(train_data_loader):
+        model.train()
         images = images.to(device)
         labels = labels.to(device)
 
@@ -115,53 +127,88 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
+        loss_train += loss.item()
 
         pred = pred.argmax(-1)
-        acc = torch.sum(pred == labels).float()
-        acc = acc / labels.size(0)
+        acc_train += (pred == labels).sum().item()
+        total_train += labels.size(0)
 
-        loss_train += loss.item()
-        acc_train += acc.item()
+        # acc_train += acc.item()
 
-        if (i + 1) % 100 == 0:
+        # if (i + 1) % 100 == 0:
             # print('Epoch [{}/{}], Step [{}/{}], loss:{:.4f}, acc:{:.4f}'
             #       .format(epoch+1, num_epochs,
             #               i + 1, total_step,
             #               loss.item(), acc_train.item()))
-            print('Epoch [{}/{}], Step [{}/{}], loss:{:.4f}, acc:{:.4f}'
-                  .format(epoch + 1, num_epochs,
-                          i + 1, total_step,
-                          loss_train/i, acc_train/i))
-
+    print('Epoch [{}/{}], Step [{}/{}], loss:{:.4f}, acc:{:.4f}'
+          .format(epoch + 1, num_epochs,
+                  i + 1, num_train_step,
+                  loss_train/num_train_step,
+                  100.*acc_train/num_train))
 
     with torch.no_grad():
-        arr = torch.zeros((len(classes), len(classes)), dtype=torch.long)
+        model.eval()
+        loss_test = 0
+        acc_test = 0
+
+        labels_test = []
+        preds_test = []
 
         for i, (images, labels) in enumerate(test_data_loader):
             images = images.to(device)
             labels = labels.to(device)
 
             pred = model(images).to(device)
+            loss = criterion(pred, labels)
+            loss_test += loss.item()
 
             pred = pred.argmax(-1)
-            acc_test = torch.sum(pred == labels)
-            acc_test = acc_test / labels.size(0)
+            acc_test += (pred == labels).sum().item()
 
-            arr += confusion_matrix(labels.cpu(), pred.cpu())
+            labels_test += labels.tolist()
+            preds_test += pred.tolist()
 
-        tb.add_scalar(global_step=epoch+1, tag='loss', scalar_value=loss.item())
-        tb.add_scalars(global_step=epoch+1,
-                       main_tag='acc',
-                       tag_scalar_dict={'train': acc_train,
-                                        'test': acc_test})
 
-        class_names = [f'{i}' for i in range(10)]
-        df_cm = pd.DataFrame(arr.cpu().numpy(), class_names, class_names)
+        # print(loss_test / num_test_step)
+        # print(acc_test / num_test)
+        # print(labels_test, preds_test)
+        # print(confusion_matrix(labels_test, preds_test))
+            # print(labels, pred)
+            # arr += confusion_matrix(labels.cpu(), pred.cpu())
+        # print(labels_test)
+        # arr = confusion_matrix(labels_test, preds_test)
+        # print(arr)
+        # print(acc_test)
 
-        fig = plt.figure(figsize=(9, 6))
-        sns.heatmap(df_cm, annot=True, fmt="d", cmap='BuGn')
-        plt.xlabel("prediction")
-        plt.ylabel("label (ground truth)")
-        plt.tight_layout()
-        tb.add_figure(tag='confusion_matrix', global_step=epoch + 1, figure=fig)
-        # plt.close(fig)
+    loss_train /= num_train_step
+    loss_test /= num_test_step
+    acc_train /= num_train
+    acc_test /= num_test
+
+    tb.add_scalars(global_step=epoch+1,
+                   main_tag='loss',
+                   tag_scalar_dict={'train':loss_train,
+                                    'test':loss_test})
+    tb.add_scalars(global_step=epoch+1,
+                   main_tag='acc',
+                   tag_scalar_dict={'train': acc_train,
+                                    'test': acc_test})
+
+    arr = confusion_matrix(labels_test, preds_test)
+    class_names = [i for i in classes.keys()]
+    df_cm = pd.DataFrame(arr, class_names, class_names)
+
+    fig = plt.figure(figsize=(9, 6))
+    sns.heatmap(df_cm, annot=True, fmt="d", cmap='BuGn')
+    plt.xlabel("prediction")
+    plt.ylabel("label (ground truth)")
+    plt.tight_layout()
+    tb.add_figure(tag='confusion_matrix', global_step=epoch + 1, figure=fig)
+    # plt.close(fig)
+
+    if best_loss > loss_test:
+        save_path = f'../../weights/resnet18/test1_{loss_test}.pth'
+        if not os.path.exists(os.path.split(save_path)[0]):
+            os.makedirs(os.path.split(save_path)[0])
+        torch.save(model.state_dict(), save_path)
+        best_loss = loss_test
--- src/gan/DCGAN.py
+++ src/gan/DCGAN.py
@@ -4,21 +4,24 @@ import torchvision
 import torch.nn as nn
 from torchvision import transforms
 from torchvision.utils import save_image
-from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sys
+sys.path.append('..')
+from utiles.tensorboard import getTensorboard
+from utiles.data import getSubDataset
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('device:', device)
 
 # TensorBoard define
-comment = 'test1'
-log_dir = '../tb_logs/DCGAN/Test1'
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-tb = SummaryWriter(log_dir=log_dir, comment=comment)
+tensorboard_path = '../../tb_logs/DCGAN/test1'
+tb = getTensorboard(log_dir=tensorboard_path)
 
 # Hyper-parameters
-nc=1
+nc=3
 nz=100
 ngf=32
 ndf=32
@@ -27,7 +30,7 @@ latent_size = 64
 hidden_size = 256
 image_size = 64
 num_epochs = 200
-batch_size = 128
+batch_size = 64
 learning_rate = 0.0002
 beta1 = 0.5
 beta2 = 0.999
@@ -36,38 +39,75 @@ ngpu = 1
 
 fixed_noise = torch.randn((64, nz, 1, 1)).to(device)
 
-sample_dir = '../samples'
+# sample_dir = '../samples'
 # Create a directory if not exists
-if not os.path.exists(sample_dir):
-    os.makedirs(sample_dir)
+# if not os.path.exists(sample_dir):
+#     os.makedirs(sample_dir)
 
 def denorm(x):
     out = (x + 1) / 2
     return out.clamp(0, 1)
 
 # Image processing
-# transform = transforms.Compose([
-#                 transforms.ToTensor(),
-#                 transforms.Normalize(mean=(0.5, 0.5, 0.5),   # 3 for RGB channels
-#                                      std=(0.5, 0.5, 0.5))])
-
 transform = transforms.Compose([
-    transforms.Resize(image_size),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5],  # 1 for greyscale channels
-                         std=[0.5])])
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.5, 0.5, 0.5),   # 3 for RGB channels
+                                     std=(0.5, 0.5, 0.5))])
+#
+# transform = transforms.Compose([
+#     transforms.Resize(image_size),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.5],  # 1 for greyscale channels
+#                          std=[0.5])])
 
-# MNIST dataset
-mnist = torchvision.datasets.MNIST(root='../data/',
+# # MNIST dataset
+# mnist = torchvision.datasets.MNIST(root='../data/',
+#                                    train=True,
+#                                    transform=transform,
+#                                    download=True)
+#
+# # Data loader
+# data_loader = torch.utils.data.DataLoader(dataset=mnist,
+#                                           batch_size=batch_size,
+#                                           shuffle=True)
+
+
+# Dataset define
+train_dataset = torchvision.datasets.CIFAR10(root='../../data/',
                                    train=True,
                                    transform=transform,
                                    download=True)
 
+test_dataset = torchvision.datasets.CIFAR10(root='../../data/',
+                                   train=False,
+                                   transform=transform,
+                                   download=True)
+
 # Data loader
-data_loader = torch.utils.data.DataLoader(dataset=mnist,
+data_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                           batch_size=batch_size,
                                           shuffle=True)
 
+# Dataset modify
+classes = {'plane':0, 'car':1, 'bird':2, 'cat':3,
+           'deer':4, 'dog':5, 'frog':6, 'horse':7, 'ship':8, 'truck':9}
+labels = torch.tensor(train_dataset.train_labels)
+ratio = [0.5**i for i in range(len(classes))]
+# print(classes)
+# print(labels)
+# print(ratio)
+transformed_dataset, count = getSubDataset(dataset=train_dataset,
+                                     class_index=classes,
+                                     labels=labels,
+                                     lratio=ratio)
+fig = plt.figure(figsize=(9, 6))
+sns.barplot(
+    data=count,
+    x="class",
+    y="original"
+)
+plt.tight_layout()
+tb.add_figure(tag='original_data_dist', figure=fig)
 
 # custom weights initialization called on netG and netD
 def weights_init(m):
@@ -86,22 +126,22 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
+            # nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            # nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # state size. (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
@@ -115,11 +155,11 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
+            # nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            # nn.BatchNorm2d(ngf * 8),
+            # nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 4, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
@@ -142,6 +182,20 @@ class Generator(nn.Module):
 # Device setting
 D = Discriminator(ngpu).to(device)
 G = Generator(ngpu).to(device)
+
+# temp_in = torch.randn((10,3,32,32)).to(device)
+# for i in D.modules():
+#     if 'Conv2d' == i.__class__.__name__:
+#         y = i(temp_in)
+#         print(y.size())
+#         temp_in = y
+# print('\n\n')
+# temp_in = torch.randn((10,100,1,1)).to(device)
+# for i in G.modules():
+#     if 'ConvTranspose2d' == i.__class__.__name__:
+#         y = i(temp_in)
+#         print(y.size())
+#         temp_in = y
 
 D.apply(weights_init)
 G.apply(weights_init)
@@ -220,7 +274,6 @@ for epoch in range(num_epochs):
         g_loss.backward()
         g_optimizer.step()
 
-
         if (i + 1) % 200 == 0:
             print('Epoch [{}/{}], Step [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f}'
                   .format(epoch+1, num_epochs, i + 1, total_step, d_loss.item(), g_loss.item(),
@@ -231,10 +284,18 @@ for epoch in range(num_epochs):
     #     images = images.reshape(images.size(0), 1, 28, 28)
     #     save_image(denorm(images), os.path.join(sample_dir, 'real_images.png'))
 
-    tb.add_scalar(tag='d_loss', global_step=epoch+1, scalar_value=d_loss.item())
-    tb.add_scalar(tag='g_loss', global_step=epoch+1, scalar_value=g_loss.item())
-    tb.add_scalar(tag='real_score', global_step=epoch+1, scalar_value=real_score.mean().item())
-    tb.add_scalar(tag='fake_score', global_step=epoch+1, scalar_value=fake_score.mean().item())
+    tb.add_scalars(global_step=epoch + 1,
+                   main_tag='loss',
+                   tag_scalar_dict={'discriminator':d_loss.item(),
+                                    'generator':g_loss.item()})
+    tb.add_scalars(global_step=epoch + 1,
+                   main_tag='score',
+                   tag_scalar_dict={'real':real_score.mean().item(),
+                                    'fake':fake_score.mean().item()})
+    # tb.add_scalar(tag='d_loss', global_step=epoch+1, scalar_value=d_loss.item())
+    # tb.add_scalar(tag='g_loss', global_step=epoch+1, scalar_value=g_loss.item())
+    # tb.add_scalar(tag='real_score', global_step=epoch+1, scalar_value=real_score.mean().item())
+    # tb.add_scalar(tag='fake_score', global_step=epoch+1, scalar_value=fake_score.mean().item())
 
     result_images = denorm(G(fixed_noise))
     tb.add_images(tag='gened_images', global_step=epoch+1, img_tensor=result_images)
