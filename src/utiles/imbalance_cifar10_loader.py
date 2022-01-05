f02479afe0f9a36a1ddc326c076cf560663fb647
sinaenjuni <y2k4388@gmail.com>
2022-01-05 20:20:33 +0900

    0105:20

--- src/utiles/imbalance_cifar10_loader.py
+++ src/utiles/imbalance_cifar10_loader.py
@@ -54,9 +54,9 @@ class ImbalanceCIFAR10DataLoader(DataLoader):
         normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                                          std=[0.2023, 0.1994, 0.2010])
         train_trsfm = transforms.Compose([
-            # transforms.RandomCrop(32, padding=4),
-            # transforms.RandomHorizontalFlip(),
-            # transforms.RandomRotation(15),
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
             transforms.ToTensor(),
             normalize,
         ])
